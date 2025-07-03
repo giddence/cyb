75687c5ac992dd2fc132c4b49030b5045d270b0d
giddence <70460481+giddence@users.noreply.github.com>
2025-07-03 22:18:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_b2_value():
     client = gspread.authorize(creds)
 
     # 打开表格（替换为你的 SHEET_ID 和工作表名称）
-    sheet = client.open_by_key("SHEET_ID").worksheet("工作表1")
+    sheet = client.open_by_key("1XttIKCNl3-PeZF1Y4-P4hQ-zXo0NMbmPAXQwm53ipS8").worksheet("工作表1")
     b2_value = sheet.acell("B2").value  # 读取 B2 单元格
     print(f"B2 的值: {b2_value}")
     return b2_value
